f7132026e03e9a4533dedc1075c0aacfbabfa227
jmenken <julia.menken@gmail.com>
2019-06-18 16:36:13 -0800

    small changes

--- utils/datahandler/man_reader.py
+++ utils/datahandler/man_reader.py
@@ -1,62 +1,52 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Fri Jun  7 11:07:46 2019
 
-@author: julia
-"""
-
-import numpy as np
-import xarray as xr
 import glob
 import os
-import sys
 import pandas as pd
+import numpy as np
+import xarray as xr
+from .MainReader import MainReader
 
-class man_reader():
+class MANReader(MainReader):
     
-    def __init__(self, path="", pathout="", aod_sda="AOD", sampled="daily",
-                 level=20):
-        if sampled not in ["all_points", "daily", "series"]:
-            print("Sampled must be either 'all_points', 'daily' or 'series'.")
-        if level not in [10, 15, 20]:
-            print("Level must be either 10, 15 or 20.")
-        
-        self.path = path
-        self.pathout = pathout
-        self.aod_sda = aod_sda
+    def __init__(self, sampled="daily", level=20):
+        super().__init__()
         self.sampled = sampled
         self.level = level
-        self.file_nc = os.path.join(self.pathout, "","{}_{}_lev{}.nc".format(
-                                    self.aod_sda, self.sampled, self.level))
     
-    def read_man(self):
-        # files containing data
-        if self.aod_sda == "AOD":
-            str_files = "*_{}.lev{}".format(self.sampled, self.level)
-        filepath = os.path.join(self.path, "", self.aod_sda, "")
-        filelist = sorted(glob.glob(filepath + str_files))
+    def __check_init(self):
+        if self.sampled not in ["all_points", "daily", "series"]:
+            raise ValueError("Sampled must be either 'all_points', 'daily' or 'series'.")
+        if self.level not in [10, 15, 20]:
+            raise ValueError("Level must be either 10, 15 or 20.")
+    
+    def __assign_aod550(self, df):
+        return df.assign(AOD_550nm=(- df["440_870nm_Angstrom_Exponent"]
+                                    * np.log(.55 / .44)
+                                    + (df["AOD_440nm"]).apply(np.log)
+                                    ).apply(np.exp))
+    
+    def __dateparser(self):
+        return lambda x: pd.Timestamp.strptime(x, '%d:%m:%Y %H:%M:%S')
         
-        # set dateparser and parsedates to read in dates
-        dateparser = lambda x: pd.Timestamp.strptime(x, '%d:%m:%Y %H:%M:%S')
-        parsedates = {"Datetime": [0,1]}
-        # check if there are any files to read
+    def read_data(self):    
+        self.__check_init()
+        
+        filelist = sorted(glob.glob(os.path.join(self.config["MANDATA"]["PATH"])
+                                    + "*_{}.lev{}".format(self.sampled, self.level)))
         if not filelist:
-            print("No files to read. Please make other specifications.")
-            sys.exit()
-        # iterate over files and create pd.Dataframe
-        for file in filelist:
-            temp = pd.read_csv(file, sep=",", header=4,
-                               parse_dates=parsedates,
-                               date_parser=dateparser,
-                               na_values=-999.000000)
-            try:
-                df = pd.concat([df, temp], axis=0, sort=True)
-            except NameError:
-                df = temp
+            raise ValueError("No files to read. Please make other selections.")
         
-        # check in column names for spaces
+        df = pd.concat((pd.read_csv(file, sep=",", header=4,
+                                    parse_dates={"Datetime": [0,1]},
+                                    date_parser=self.__dateparser(),
+                                    na_values=-999.000000)
+                        for file in filelist), axis=0, sort=True)
+        
         df.rename(lambda x: x.replace(" ", "_"), axis="columns", inplace=True)
+        df.rename(lambda x: x.replace("-", "_") , axis="columns", inplace=True)
+        df = self.__assign_aod550(df)
         
         if self.sampled == "daily":
             df.Datetime = df.Datetime.apply(lambda x: x.replace(hour=0,
@@ -66,7 +56,6 @@ class man_reader():
             df = df.drop(columns=["Date(dd:mm:yyyy)", "Time(hh:mm:ss)",
                                   "Last_Processing_Date(dd:mm:yyyy)"])
         
-        # create MultiIndex and sort
         df.set_index(["Datetime", "Latitude", "Longitude"], inplace=True)
         df.sort_index(ascending=True, inplace=True)
         
@@ -74,33 +63,32 @@ class man_reader():
     
     def df_to_ds(self, df):
         # create xr.Dataset from pd.Dataframe
-        ds = xr.Dataset(df)
-        
-        return ds
+        return xr.Dataset(df)
     
     def write_ds_to_nc(self, ds):
         # writes xr.Dataset with multiindex to netcdf
+        file_nc = os.path.join(self.config["MANDATA"]["PATHNC"],
+                               "{}_lev{}.nc".format(self.sampled, self.level))
         ds = ds.reset_index("dim_0")
-        ds.to_netcdf(self.file_nc, mode="w")
+        ds.to_netcdf(file_nc, mode="w")
     
     def read_ds_from_nc(self):
         # reads xr.Dataset from netcdf and constructs multiindex
-        ds = xr.open_dataset(self.file_nc)
-        ds = ds.set_index(dim_0=["Datetime", "Latitude", "Longitude"])
+        file_nc = os.path.join(self.config["MANDATA"]["PATHNC"],
+                               "{}_lev{}.nc".format(self.sampled, self.level))
+        ds = xr.open_dataset(file_nc)
+        return ds.set_index(dim_0=["Datetime", "Latitude", "Longitude"])
         
-        return ds
         
-    def main(self):
-        df = man.read_man()
-        ds = man.df_to_ds(df)
-        man.write_ds_to_nc(ds)
-#        ds_new = man.read_ds_from_nc()
-        
-        return ds
+def main():
+    man = MANReader()
+    df = man.read_data()
+    ds = man.df_to_ds(df)
+    man.write_ds_to_nc(ds)
+    
+    return ds
     
 if __name__ == "__main__":
-    path = "/Users/julia/Documents/MPI_Sonne/microtops/data/All_MAN_Data/"
-    man = man_reader(path, path, "AOD", "daily", 20)
-    ds = man.main()
+    man_ds = main()
     
     
