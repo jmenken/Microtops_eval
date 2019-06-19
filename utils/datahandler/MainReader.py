6414ee360426857586a79f3eadc9b5c028e1ebf6
jmenken <julia.menken@gmail.com>
2019-06-18 16:35:42 -0800

    Path to Path.ini adjusted

--- utils/datahandler/MainReader.py
+++ utils/datahandler/MainReader.py
@@ -7,7 +7,7 @@ class MainReader(object):
     def __init__(self):
         self.config = self.__read_config()
 
-    def __read_config(self, file="../../PATH.ini"):
-        config_reader = configparser.ConfigParser(interpolation=ExtendedInterpolation())
-        config = config_reader.read(os.path.abspath(file))
+    def __read_config(self, file=os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', '..', 'PATH.ini'))):
+        config = configparser.ConfigParser(interpolation=ExtendedInterpolation())
+        config.read(os.path.abspath(file))
         return config
